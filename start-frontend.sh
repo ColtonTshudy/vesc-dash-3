75ae07dba5c81898e8dd3ba45bef5af622c00b6e
ColtonTshudy <82547877+ColtonTshudy@users.noreply.github.com>
2023-08-19 23:12:04 -0400

    whoops forgot start sh

---
+++ start-frontend.sh
@@ -0,0 +1,8 @@
+DISPLAY=:0 xdotool mousemove 2000 2000
+cd /home/tomos/Projects/VESCDash/vesc-dash-3
+npm run estart #Electron renderer
+
+# DISPLAY=:0 firefox --kiosk http://localhost:5051 #Browser renderer
+
+# Add this (../build <here> ) & if you want to use browser renderer 
+#; sudo -H -u tomos /bin/python -m http.server 5051
